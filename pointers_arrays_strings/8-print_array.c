effeeec980bda3c5d3b31c297e88bb1ae9d586e4
Nick <nicholasyoung548@yahoo.com>
2023-06-13 19:09:17 -0700

    Ganonmum

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-#include <stlib.h>
+
 /**
+ * main - check the code for
  * print_array - a function that prints n elements of an array
  * @a: array name
  * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
  */
+
 void print_array(int *a, int n)
 {
 	int i;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,12 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -23,3 +29,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+
+#endif
